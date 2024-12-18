bc8800950d2d91085db2e2f69d3156c73d691770
NickelBlvck <r3dix0r@yandex.ru>
2024-12-18 15:17:21 +0300

    Update Zabbix-agent-2.sh

--- Zabbix-agent-2.sh
+++ Zabbix-agent-2.sh
@@ -10,10 +10,12 @@ ZABBIX_CONF="/etc/zabbix/zabbix_agent2.conf"
 
 # Настроим сервер и активный сервер
 echo "Настраиваем Server и ServerActive..."
-sudo sed -i 's/^#Server=127.0.0.1/Server=zabbix.kuznecoff.tech/' $ZABBIX_CONF
-sudo sed -i 's/^#ServerActive=127.0.0.1/ServerActive=zabbix.kuznecoff.tech/' $ZABBIX_CONF
+# Заменяем строку с Server
+sudo sed -i 's/^Server=[^ ]*/Server=zabbix.kuznecoff.tech/' $ZABBIX_CONF
+# Заменяем строку с ServerActive
+sudo sed -i 's/^ServerActive=[^ ]*/ServerActive=zabbix.kuznecoff.tech/' $ZABBIX_CONF
 
-# Добавляем параметры для Zabbix Agent 2
+# Добавляем параметры для Zabbix
 echo "Добавляем UserParameter для fail2ban и ssh.port..."
 echo "UserParameter=service.status.fail2ban,/usr/local/bin/check_fail2ban.sh" | sudo tee -a $ZABBIX_CONF > /dev/null
 echo "UserParameter=ssh.port,/usr/local/bin/get_ssh_port.sh" | sudo tee -a $ZABBIX_CONF > /dev/null
@@ -25,13 +27,20 @@ sudo systemctl restart zabbix-agent2
 # Устанавливаем скрипты для проверки fail2ban и порта SSH
 echo "Загружаем скрипты из Git репозитория..."
 sudo apt-get install -y git
-sudo git clone https://github.com/NickelBlvck/check_fail2ban /usr/local/bin/check_fail2ban.sh
-sudo git clone https://github.com/NickelBlvck/get_ssh_port /usr/local/bin/get_ssh_port.sh
-
-# Делаем скрипты исполнимыми
-echo "Делаем скрипты исполнимыми..."
+# Скачиваем файл check_fail2ban.sh
+sudo wget -O /usr/local/bin/check_fail2ban.sh https://raw.githubusercontent.com/NickelBlvck/check_fail2ban/refs/heads/main/check_fail2ban.sh
+# Скачиваем файл get_ssh_port.sh
+sudo wget -O /usr/local/bin/get_ssh_port.sh https://raw.githubusercontent.com/NickelBlvck/get_ssh_port/refs/heads/main/get_ssh_port.sh
+# Делаем их исполнимыми
 sudo chmod +x /usr/local/bin/check_fail2ban.sh
 sudo chmod +x /usr/local/bin/get_ssh_port.sh
+
+# Перезапуск сервиса Zabbix Agent 2
+echo "Перезапускаем Zabbix Agent 2..."
+sudo systemctl restart zabbix-agent2
+
+# Проверка статуса
+sudo systemctl status zabbix-agent2
 
 # Все готово
 echo "Zabbix Agent 2 установлен и настроен."
